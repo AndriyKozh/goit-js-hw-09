8bfdb09c5b57b0cbd4f7e32ad985be79fa8677d8
AndriyKozh <max190716@ukr.net>
2022-08-12 12:56:24 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+let timerId = null;
+
+refs.start.addEventListener('click', onClickStart);
+refs.stop.addEventListener('click', onClickStop);
+
+function colorRandom() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onClickStart() {
+  timerId = setInterval(colorRandom, 2000);
+}
+
+function onClickStop() {
+  clearInterval(timerId);
+}
