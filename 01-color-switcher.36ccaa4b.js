a81738d0f838460f21af989fde275fb022f56254
AndriyKozh <98039703+AndriyKozh@users.noreply.github.com>
2022-08-12 10:39:29 +0000

    Deploying to gh-pages from @ AndriyKozh/goit-js-hw-09@43df0d9e057b82426bf578acc37a9eb6738b1512 🚀

---
+++ 01-color-switcher.36ccaa4b.js
@@ -0,0 +1,2 @@
+!function(){const t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]"),body:document.querySelector("body")};let o=null;function e(){t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.start.addEventListener("click",(function(){o=setInterval(e,2e3)})),t.stop.addEventListener("click",(function(){clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.36ccaa4b.js.map
